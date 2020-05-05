97a4a77b68f7356e34d5bfe7729779507b131e3a
Omar Santana <osantana@shaynet.shaydomain>
2020-05-05 17:06:15 +0200

    add dynamic GET route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+
 const mockUserData=[
 {name:'Mark'},
 {name:'Jill'}
@@ -12,6 +13,15 @@ app.get('/users', function(req,res){
  	 	message: 'successfully got users. Nice!',
  	 	users: mockUserData
  	})
+})
+
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
 })
 
 app.listen(8000,function(){console.log('server is listening')})
